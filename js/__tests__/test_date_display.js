b216196cd271d4e95252be74bdda08546c4c4d9c
Gabriel Birke <gb@birke-software.de>
2015-05-11 22:57:56 +0200

    Localize DateDisplay
    Add moment.js to project.
    Use the German locale in component and tests.
    Modify package.json to package moment.js with locales when compiling the
    app with browserify.

--- js/__tests__/test_date_display.js
+++ js/__tests__/test_date_display.js
@@ -1,4 +1,6 @@
 jest.dontMock('../components/DateDisplay.js');
+jest.dontMock('moment');
+
 describe("DateDisplay", function(){
 	it("shows current date", function() {
 		var React = require("react/addons");
@@ -6,13 +8,16 @@ describe("DateDisplay", function(){
 		var DateGenerator = require("../utils/DateGenerator");
 		var TestUtils = React.addons.TestUtils;
 
-		DateGenerator.now.mockReturnValueOnce(new Date("2015-05-04 03:02:01 GMT"));
+		DateGenerator.now.mockReturnValueOnce(new Date("2015-05-04 03:02:01"));
 
 		var display = TestUtils.renderIntoDocument(
 			<DateDisplay />
 			);
 		
-		var dateSpan = TestUtils.findRenderedDOMComponentWithTag(display, "span");
-		expect(dateSpan.getDOMNode().textContent).toEqual("Mon, 04 May 2015 03:02:01 GMT");
+		var timeSpan = TestUtils.findRenderedDOMComponentWithTag(display, "h3");
+		var dateSpan = TestUtils.findRenderedDOMComponentWithTag(display, "h4");
+
+		expect(timeSpan.getDOMNode().textContent).toEqual("03:02:01");
+		expect(dateSpan.getDOMNode().textContent).toEqual("4. Mai 2015");
 	});
 });
--- js/components/DateDisplay.js
+++ js/components/DateDisplay.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var DateGenerator = require('../utils/DateGenerator');
+var moment = require('moment');
 
 var DateDisplay = React.createClass({
 	getInitialState: function() {
@@ -8,8 +9,14 @@ var DateDisplay = React.createClass({
 		};
 	},
 	render: function() {
-		var datestring = this.state.date.toGMTString();
-		return <div><span>{datestring}</span></div>
+		var m = moment(this.state.date).locale("de"),
+			timestring = m.format("LTS"),
+			datestring = m.format("LL");
+
+		return <div>
+				<h3>{timestring}</h3>
+				<h4>{datestring}</h4>
+			</div>
 	}
 });
 
